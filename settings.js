2c8d48cd472141ad0d25ad3e20ed4304cb2a000b
Geeth423 <108218873+Geeth423@users.noreply.github.com>
2022-06-26 16:27:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+94778490045']
+global.premium = ['+94778490045']
+global.ownernomer = '+94778490045'
+global.ownername = '🤖Geeth'
+global.botname = '🤖Geeth👿'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'wt🐼 sticker'
+global.author = '🐦Geeth'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
